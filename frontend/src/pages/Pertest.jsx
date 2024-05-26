2b942cfae1a5bcc578ff8b1f38f9d255787de5eb
Mikiqueen0 <mikiqueen2004@gmail.com>
2024-05-27 03:19:55 +0700

    connect ML to Pertest page

--- frontend/src/pages/Pertest.jsx
+++ frontend/src/pages/Pertest.jsx
@@ -1,6 +1,23 @@
 import Sidebar from '../components/Sidebar';
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function Pertest({ showSidebar }){
+    const [msg, setMsg] = useState("");
+    const [MBTI, setMBTI] = useState("");
+    const getPredictResult = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://127.0.0.1:5000/predict', { message: msg });
+            const predictedResult = response.data.prediction;
+            console.log("Type:", predictedResult);
+            setMBTI(predictedResult);
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    }
+    
+
     return(
         <div className=" w-full h-screen flex flex-col items-center border-black bg-[url('./assets/images/cartoon-bg.png')] bg-cover bg-fixed font-nunito ">
             <Sidebar showSidebar={showSidebar}/>
@@ -17,13 +34,13 @@ export default function Pertest({ showSidebar }){
                 <label className="text-[25px] font-semibold text-white mt-3"> 
                     Dialogue: <br />
                 </label>
-                <textarea id="message" rows="10" class="block p-5 w-full  text-[18px] font-medium text-gray-900 border border-[#CE9EFF] shadow-[rgba(223,_28,_255,_0.6)_0px_0px_20px] rounded-[20px] bg-gradient-to-r from-[#FFFFFF] from-0% to-[#B770FF] to-89%  "></textarea>
-                <button className='analyze  bg-white rounded-[16px] w-[70%] p-3 font-bold text-black mt-5 mb-2 px-20  transform motion-safe:hover:scale-105  ' type="button">Analyze</button>
+                <textarea id="message" rows="10" className="block p-5 w-full  text-[18px] font-medium text-gray-900 border border-[#CE9EFF] shadow-[rgba(223,_28,_255,_0.6)_0px_0px_20px] rounded-[20px] bg-gradient-to-r from-[#FFFFFF] from-0% to-[#B770FF] to-89%" onChange={e => setMsg(e.target.value)}></textarea>
+                <button className='analyze  bg-white rounded-[16px] w-[70%] p-3 font-bold text-black mt-5 mb-2 px-20  transform motion-safe:hover:scale-105  ' type="button" onClick={getPredictResult}>Analyze</button>
                 <p className='text-white'>
                     Analysis:
                 </p>
                 <div className='flex justify-center h-full w-[120%] border-2 border-primary-pink rounded-[30px] bg-gradient-to-r from-[#FFFFFF] from-0% to-[#B770FF] to-89%  '>
-                    <p className='text-[25px] font-semibold '>MBTI</p>
+                    <p className='text-[25px] font-semibold'>{MBTI}</p>
                 </div>
             </div>
 
---
+++ server/ml_model/ml_server.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from joblib import load
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import numpy as np
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+model = load('server/ml_model/model.sav')
+
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
+
+# Preprocess text
+def preprocess_text(text):
+    # lowercase
+    text = text.lower()
+    #remove tagged name
+    text = re.sub(r'@\w+\s*', '', text)
+    # remove numbers
+    text = re.sub(r'\d+', '', text)
+    # remove contractions
+    text = re.sub(r'\b(\w+)(\'s|\'ve|\'d)\b', r'\1', text)
+    # remove special characters
+    text = re.sub(r'[_;:()]', '', text)
+    #remove puncutation
+    text = re.sub(r'[^\w\s]', '', text)
+    # Tokenize words
+    words = nltk.word_tokenize(text)
+    # Lemmatize words
+    lemmatized_words = [lemmatizer.lemmatize(word) for word in words]
+    # remove stopword
+    words = [word for word in lemmatized_words if word not in stop_words and word != '...']
+    # join items in list into single string
+    processed_text = ' '.join(words)
+    print(processed_text)
+    return processed_text
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    req = request.get_json()
+    message = req['message']
+    prediction = model.predict([preprocess_text(message)])
+    prediction_list = prediction.tolist()
+    return jsonify({'prediction': prediction_list})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
